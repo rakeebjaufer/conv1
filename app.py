30294772a033e2e0c2b9c5c92a181bd825a76185
Rakeeb Jaufer <rakeebmj@hotmail.com>
2022-09-13 15:05:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def main():
     with st.sidebar:
         st.markdown("---")
         st.markdown(
-            '<h6>Made in &nbsp<img src="https://streamlit.io/images/brand/streamlit-mark-color.png" alt="Streamlit logo" height="16">&nbsp by <a href="https://twitter.com/andfanilo">@andfanilo</a></h6>',
+            '<h6>Made in &nbsp<img src="https://streamlit.io/images/brand/streamlit-mark-color.png" alt="Streamlit logo" height="16">&nbsp by <a href="https://twitter.com/">@rj</a></h6>',
             unsafe_allow_html=True,
         )
         st.markdown(
